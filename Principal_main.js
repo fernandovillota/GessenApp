2bc5640d9af63facaac2807eb5ac10fe5ecfdbd3
GessenApp <fernandovillotaeraso>
2024-05-31 20:05:57 -0500

    confirmar:p

--- Principal_main.js
+++ Principal_main.js
@@ -4,6 +4,6 @@ function mostrarContenido(id) {
     for (var i = 0; i < contenidos.length; i++) {
         contenidos[i].style.display = 'none';
     }
-    // Muestra el contenido correspondiente al ID proporcionado
+    // Muestra el contenido correspondiente al ID proporcionado 
     document.getElementById(id).style.display = 'block';
 }
