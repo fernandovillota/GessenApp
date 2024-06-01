6fe9183be23f3899285a04292c14fc5f4812f226
StrangerIce09 <alexanderpantoja32004@gmail.com>
2024-06-01 17:22:47 -0500

    filstros

--- Principal_main.js
+++ Principal_main.js
@@ -7,3 +7,23 @@ function mostrarContenido(id) {
     // Muestra el contenido correspondiente al ID proporcionado 
     document.getElementById(id).style.display = 'block';
 }
+//function filtros(){
+//  document.getElementById("vegetariano").style.display="none";
+//}
+
+const miCheckbox = document.getElementById('checkveg');
+
+// Función que se ejecuta al cambiar el estado del checkbox
+function manejarCheckbox() {
+    if (miCheckbox.checked) {
+        document.getElementById("vegetariano").style.display = "none";
+        // Acción cuando el checkbox está marcado
+        console.log('Checkbox marcado');
+        // Aquí puedes llamar a la función que desees ejecutar
+        // cuando el checkbox está marcado.
+    } else {
+        document.getElementById("vegetariano").style.display = "block";
+        // Acción cuando el checkbox está desmarcado
+    }
+}
+miCheckbox.addEventListener('change', manejarCheckbox);
